5e19bb21f3cebb90701587071e511e0db8a9a47b
Kuba <jakubkcichocki@gmail.com>
2021-03-28 16:30:37 +0200

    update person

--- firebaseTest.py
+++ firebaseTest.py
@@ -11,3 +11,7 @@ print(reportService.get_all_reports())
 #person existence check
 print(personService.check_person_existence("Jan", "Kowalski", 123456789))
 print(personService.check_person_existence("Jan", "Kowalski", 123256789))
+
+#person data change check
+id_changed = personService.check_person_existence("Jan", "Kowalski", 123456789)
+personService.change_person_data(id_changed, "Jan", "Kowalski2", 666666666, 1, 1, 1, 1)
