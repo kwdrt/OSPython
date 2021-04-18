3ac97b35e7021f17203beac0313035dd48ee0d4c
Kuba <jakubkcichocki@gmail.com>
2021-04-18 18:19:33 +0200

    Test for raport generation added, will be rewritten into a functional module with minor corrections

--- firebaseTest.py
+++ firebaseTest.py
@@ -54,30 +54,68 @@ print(printed_report)
 font_dir = os.getcwd() + '\\font\\'
 
 def single_data(pdf_file, text1, text2):
-    pdf.set_font('Roboto', 'BI', 14)
+    pdf.set_font('Roboto', 'BI', 12)
     pdf_file.cell(40, 10, text1)
-    pdf_file.ln(15)
-    pdf_file.set_font('Roboto', "", 10)
-    pdf_file.cell(40, 10, text2)
-    pdf_file.ln(20)
+    pdf_file.cell(40)
+    pdf_file.set_font('Roboto', "", 8)
+    pdf_file.cell(0, 10, text2)
+    pdf_file.ln(10)
 
+def long_data(pdf_file, text1, text2):
+    pdf.set_font('Roboto', 'BI', 12)
+    pdf_file.cell(40, 10, text1)
+    pdf_file.set_font('Roboto', "", 8)
+    pdf_file.ln(10)
+    pdf_file.multi_cell(0,5,text2)
+    pdf_file.ln()
+
+def id_to_text(ps, id):
+    person = ps.get_person_by_id(id)
+    person_data = person.get("FirstName") + " " + person.get("LastName")
+    return person_data
+
+def section_to_string(ps, section_table):
+    section_string = ""
+    for id in section_table:
+        section_string += id_to_text(ps, id) + "\n"
+    return section_string
 
 pdf = fpdf.FPDF()
 pdf.add_page()
+
+#pdf.multi_cell()
 
 pdf.add_font("Roboto", style="", fname= font_dir + "Roboto-Regular.ttf", uni=True)
 pdf.add_font("Roboto", style="B", fname= font_dir + "Roboto-Bold.ttf", uni=True)
 pdf.add_font("Roboto", style="I", fname= font_dir + "Roboto-Italic.ttf", uni=True)
 pdf.add_font("Roboto", style="BI", fname= font_dir + "Roboto-BoldItalic.ttf", uni=True)
 
-pdf.set_font('Roboto', 'BI', 14)
-pdf.cell(80)
-pdf.cell(30, 10, 'Raport', 1, 0, 'C')
+pdf.set_font('Roboto', 'BI', 8)
+pdf.cell(0, 10, 'Raport z dnia ' + printed_report.get("out_date"), 1, 0, 'C')
 pdf.ln(20)
 
-single_data(pdf, "Nagłówek sekcji", "dane nizej pod naglowkiem")
-single_data(pdf, u"""Nagłówek sekcji""", "dane nizej pod naglowkiem")
-single_data(pdf, u"""Nagłówek sekcji""", "dane nizej pod naglowkiem")
+single_data(pdf, "Data wyjazdu", printed_report.get("out_date"))
+single_data(pdf, "Godzina wyjazdu", printed_report.get("out_hour"))
+single_data(pdf, "Godzina na miejscu", printed_report.get("at_place_hour"))
+single_data(pdf, "Rodzaj zdarzenia", printed_report.get("accident_type"))
+single_data(pdf, "Miejsce zdarzenia", printed_report.get("place_name"))
+
+# Sekcja inaczej
+long_data(pdf, "Skład sekcji", section_to_string(personService, printed_report.get("section_current")))
+
+single_data(pdf, "Dowódca sekcji", id_to_text(personService, printed_report.get("section_leader_id")))
+single_data(pdf, "Dowódca akcji", id_to_text(personService, printed_report.get("action_leader_id")))
+single_data(pdf, "Kierowca", id_to_text(personService, printed_report.get("driver_id")))
+
+single_data(pdf, "Sprawca", printed_report.get("perpetrator"))
+single_data(pdf, "Poszkodowany", printed_report.get("injured"))
+long_data(pdf, "Szczegóły zdarzenia", printed_report.get("details"))
+single_data(pdf, "Data powrotu", printed_report.get("return_date"))
+single_data(pdf, "Godzina zakończenia", printed_report.get("return_hour"))
+single_data(pdf, "Godzina w remizie", printed_report.get("depot_hour"))
+single_data(pdf, "Stan licznika", printed_report.get("counter_state"))
+single_data(pdf, "KM do miejsca zdarzenia", str(printed_report.get("KM_to_place")))
+
 
 pdf.output("testraport.pdf")
 
