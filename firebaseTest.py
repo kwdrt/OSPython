aab02c29a61e72963f3eb11ea6d67877a6bb4acc
Kuba <jakubkcichocki@gmail.com>
2021-04-18 17:14:22 +0200

    Font add for PDF gen

--- firebaseTest.py
+++ firebaseTest.py
@@ -1,5 +1,9 @@
+import os
+
 import firebaseUtils as fU
-from fpdf import FPDF
+import fpdf
+
+print()
 
 personService = fU.PersonService()
 reportService = fU.ReportService()
@@ -7,11 +11,11 @@ reportService = fU.ReportService()
 #
 #print(personService.get_all_people())
 #print("\n")
-print(reportService.get_all_reports())
+#print(reportService.get_all_reports())
 
 # person existence check
-print(personService.check_person_existence("Jan", "Kowalski2", 666666666))
-print(personService.check_person_existence("Jan", "Kowalski", 123256789))
+#print(personService.check_person_existence("Jan", "Kowalski2", 666666666))
+#print(personService.check_person_existence("Jan", "Kowalski", 123256789))
 
 # person data change check
 #id_changed = personService.check_person_existence("Jan", "Kowalski2", 666666666)
@@ -25,5 +29,55 @@ print(personService.check_person_existence("Jan", "Kowalski", 123256789))
 
 
 # -MXrGrD-KriOFQsiUMf3 to print
+
+# data wyjazdu
+# godzina wyjazdu
+# godzina na miejscu
+# rodzaj zdarzenia
+# miejsce zdarzenia
+# sekcja
+# dowódca sekcji
+# dwd akcji
+# kierowca
+# sprawca
+# poszkodowany
+# szczegóły zdarzenia
+# data powrotu
+# godzina zakończenia
+# godzina w remizie
+# stan licznika
+# KM do miejsca zdarzenia
+
 printed_report = reportService.get_report_data("-MXrGrD-KriOFQsiUMf3")
+print(printed_report)
+
+font_dir = os.getcwd() + '\\font\\'
+
+def single_data(pdf_file, text1, text2):
+    pdf.set_font('Roboto', 'BI', 14)
+    pdf_file.cell(40, 10, text1)
+    pdf_file.ln(15)
+    pdf_file.set_font('Roboto', "", 10)
+    pdf_file.cell(40, 10, text2)
+    pdf_file.ln(20)
+
+
+pdf = fpdf.FPDF()
+pdf.add_page()
+
+pdf.add_font("Roboto", style="", fname= font_dir + "Roboto-Regular.ttf", uni=True)
+pdf.add_font("Roboto", style="B", fname= font_dir + "Roboto-Bold.ttf", uni=True)
+pdf.add_font("Roboto", style="I", fname= font_dir + "Roboto-Italic.ttf", uni=True)
+pdf.add_font("Roboto", style="BI", fname= font_dir + "Roboto-BoldItalic.ttf", uni=True)
+
+pdf.set_font('Roboto', 'BI', 14)
+pdf.cell(80)
+pdf.cell(30, 10, 'Raport', 1, 0, 'C')
+pdf.ln(20)
+
+single_data(pdf, "Nagłówek sekcji", "dane nizej pod naglowkiem")
+single_data(pdf, u"""Nagłówek sekcji""", "dane nizej pod naglowkiem")
+single_data(pdf, u"""Nagłówek sekcji""", "dane nizej pod naglowkiem")
+
+pdf.output("testraport.pdf")
 
